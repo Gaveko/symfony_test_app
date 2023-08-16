8703799396d2528f39a8b4aa3b1b43edc3b8f993
Gaveko <garkavenko.vladislav.00@gmail.com>
2023-08-16 11:17:19 +0300

    Added parent field into Todo entity.

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -2,18 +2,49 @@
 
 namespace App\Controller;
 
+use App\Entity\Todo;
+use App\Repository\TodoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
 {
-    #[Route('/todo', name: 'app_todo')]
+    #[Route('/', name: 'app_todo')]
     public function index(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/TodoController.php',
         ]);
+    }
+
+    #[Route('/todos', name: 'all_todos')]
+    public function allTodos(TodoRepository $repository): JsonResponse
+    {
+        $todos = $repository->findBy(['parent' => null]);
+
+        $data = $this->getData($todos);
+
+        return $this->json($data);
+    }
+
+    private function getData($todos)
+    {
+        if ($todos === null) {
+            return [];
+        }
+        $data = [];
+
+        foreach ($todos as $todo) {
+            $data[] = [
+                'id' => $todo->getId(),
+                'title' => $todo->getTitle(),
+                'description' => $todo->getDescription(),
+                'children' => $this->getData($todo->getChildren())
+            ];
+        }
+
+        return $data;
     }
 }
--- src/Entity/Todo.php
+++ src/Entity/Todo.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TodoRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -31,6 +33,17 @@ class Todo
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
     private ?\DateTimeInterface $completedAt = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'children')]
+    private ?self $parent = null;
+
+    #[ORM\OneToMany(mappedBy: 'parent', targetEntity: self::class)]
+    private Collection $children;
+
+    public function __construct()
+    {
+        $this->children = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -105,6 +118,48 @@ class Todo
     public function setCompletedAt(?\DateTimeInterface $completedAt): static
     {
         $this->completedAt = $completedAt;
+
+        return $this;
+    }
+
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): static
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getChildren(): Collection
+    {
+        return $this->children;
+    }
+
+    public function addChild(self $child): static
+    {
+        if (!$this->children->contains($child)) {
+            $this->children->add($child);
+            $child->setParent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeChild(self $child): static
+    {
+        if ($this->children->removeElement($child)) {
+            // set the owning side to null (unless already changed)
+            if ($child->getParent() === $this) {
+                $child->setParent(null);
+            }
+        }
 
         return $this;
     }
